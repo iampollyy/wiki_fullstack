81d2a447f0a78393c7e16067fa8e2466ecc2e76f
Polina Trybialustava <3belpolin@gmail.com>
2025-10-27 14:47:18 +0200

    fix: added aliases & absent dependencies

--- frontend/src/core/router/router.tsx
+++ frontend/src/core/router/router.tsx
@@ -1,7 +1,7 @@
 import { IRoute } from "./model/IRoute"
-import {Home} from "../../pages/HomePage/Home"
-import {Articles} from "../../pages/ArticlesPage/Articles"
-import {Article} from "../../pages/ArticlePage/Article"
+import {Home} from "@pages/HomePage/Home"
+import {Articles} from "@pages/ArticlesPage/Articles"
+import {Article} from "@pages/ArticlePage/Article"
 
 export const RoutesConfig: IRoute[] = [
   {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@core": path.resolve(__dirname, "src/core"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+});
