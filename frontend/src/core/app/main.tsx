d285f297fa01c768bd4f2bb306676245396f38d1
Polina Trybialustava <3belpolin@gmail.com>
2025-10-27 13:17:50 +0200

    feat: migrated front to ts

--- frontend/src/core/app/main.tsx
+++ frontend/src/core/app/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '../styles/resetCSS.css'
-import '../styles/index.css'
+import App from './App'
+import '../../styles/resetCSS.css'
+import '../../styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+// Allow importing static assets
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+
+// You can add global types here to help the gradual migration.
