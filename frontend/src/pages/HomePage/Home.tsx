d7f92398383abf1d7dc196f6d5f3c863ca4f4583
Polina Trybialustava <3belpolin@gmail.com>
2025-10-28 22:00:19 +0200

    feat:homepage added

--- frontend/src/pages/HomePage/Home.tsx
+++ frontend/src/pages/HomePage/Home.tsx
@@ -1,7 +1,34 @@
+import { Button } from '@shared/ui/button/Button'
+import styles from './home.module.scss'
+import {useNavigate} from "react-router-dom";
+
+
 export const Home = () => {
+  const navigate = useNavigate();
+  function onGoToArticlesClick() {
+    navigate('/articles')
+
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
-};
+    <>
+      <h1 className="sr-only">Home Page</h1>
+
+      <section className={styles.home__page}>
+        <h2 className="sr-only" id="searchRes">
+          Search Results
+        </h2>
+
+        <div className={styles.home__images}>
+          <img src="./public/logo.svg" alt="" />
+          <img src="src/assets/images/subtract.svg" alt="" />  
+        </div>
+ 
+
+      <Button onClick={onGoToArticlesClick}>Go to articles <span className="material-symbols-outlined">trending_flat
+        </span>
+        </Button>
+      </section>
+    </>
+  )
+}
