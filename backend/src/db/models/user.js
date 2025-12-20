7a8fc6702d1092e24371e95336cdb0613c4fc92b
Polina Trybialustava <3belpolin@gmail.com>
2025-12-21 00:17:16 +0200

    feat: add user model

---
+++ backend/src/db/models/user.js
@@ -0,0 +1,34 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../db');
+
+class User extends Model{}
+
+User.init(
+  {
+    id:{
+      type: DataTypes.SERIAL,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firtsName:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    lastName:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    email:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    passwordHash:{ 
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  }
+)
